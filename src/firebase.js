9ad695b6355237c5a51373f34e1b4f75b58eedd9
nitishkr106 <nitishkr106@gmail.com>
2023-04-27 11:17:36 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATPpqlEOyyPA5A8FmxnR5n8VY_mmOTvQI",
+  authDomain: "nitish-chatapp.firebaseapp.com",
+  projectId: "nitish-chatapp",
+  storageBucket: "nitish-chatapp.appspot.com",
+  messagingSenderId: "733594332257",
+  appId: "1:733594332257:web:1b73341bd0ffe757f24e20"
+};
+
+export const app = initializeApp(firebaseConfig);
+
+
+
