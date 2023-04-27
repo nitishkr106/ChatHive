c741c19e21d9b20b6a38202ed38bddd42249ba43
Nitish Kumar <60603263+nitishkr106@users.noreply.github.com>
2023-04-27 11:42:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATPpqlEOyyPA5A8FmxnR5n8VY_mmOTvQI",
-  authDomain: "nitish-chatapp.firebaseapp.com",
-  projectId: "nitish-chatapp",
+  apiKey: "AIzaSyATPpqlEOyyPA5A8FmxnR5n8VY_m****",
+  authDomain: "****-chatapp.firebaseapp.com",
+  projectId: "******-chatapp",
   storageBucket: "nitish-chatapp.appspot.com",
-  messagingSenderId: "733594332257",
-  appId: "1:733594332257:web:1b73341bd0ffe757f24e20"
+  messagingSenderId: "7********7",
+  appId: "1:733594332257:web:1b73341bd0ffe757f2*****"
 };
 
 export const app = initializeApp(firebaseConfig);
